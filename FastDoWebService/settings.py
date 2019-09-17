6fb2ba04b5371fc3c66b4f51a5054e7b2b6bb0ea
DouglasMarq <douglas.marq.alves@outlook.com>
2019-09-17 16:04:01 -0300

    Disable browsable interface

--- FastDoWebService/settings.py
+++ FastDoWebService/settings.py
@@ -49,7 +49,10 @@ REST_FRAMEWORK = {
         'rest_framework.authentication.TokenAuthentication',
     ),
     'DEFAULT_PERMISSION_CLASSES': (
-        'rest_framework.permissions.IsAuthenticated', )
+        'rest_framework.permissions.IsAuthenticated', ),
+    'DEFAULT_RENDERER_CLASSES': (
+        'rest_framework.renderers.JSONRenderer',
+    ),
 }
 
 MIDDLEWARE = [
